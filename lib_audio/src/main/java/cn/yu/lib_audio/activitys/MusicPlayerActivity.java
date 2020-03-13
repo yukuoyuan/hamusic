67416a29ad3262e573cf89e3a8378de8f94692b4
于阔源 <yukuoyuan@hotmail.com>
2020-03-13 15:41:07 +0800

    提交了音乐播放界面的简单实现

--- lib_audio/src/main/java/cn/yu/lib_audio/activitys/MusicPlayerActivity.java
+++ lib_audio/src/main/java/cn/yu/lib_audio/activitys/MusicPlayerActivity.java
@@ -1,12 +1,26 @@
 package cn.yu.lib_audio.activitys;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.yu.lib_audio.R;
+import cn.yu.lib_base.activitys.BaseActivity;
 
 /**
  * Created on 2020-03-11
+ * 这是一个播放详情界面
  *
  * @author yukuoyuan
  * @link github https://github.com/yukuoyuan
  */
-public class MusicPlayerActivity extends AppCompatActivity {
+public class MusicPlayerActivity extends BaseActivity {
+
+    @Override
+    protected void initData(Bundle bundle) {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_music_player;
+    }
 }
