a59124cef2bd277ac5f67abc4b88a78ecf3a85ac
于阔源 <yukuoyuan@hotmail.com>
2020-03-18 10:17:49 +0800

    提交了过度模块的简单实现

---
+++ lib_splash/src/main/java/cn/yu/lib_splash/activitys/SplashActivity.java
@@ -0,0 +1,26 @@
+package cn.yu.lib_splash.activitys;
+
+import android.os.Bundle;
+
+import cn.yu.lib_base.activitys.BaseActivity;
+import cn.yu.lib_splash.R;
+
+/**
+ * Created on 2020-03-18
+ * 这是一个过度界面
+ *
+ * @author yukuoyuan
+ * @link github https://github.com/yukuoyuan
+ */
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void initData(Bundle bundle) {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_splash;
+    }
+}
